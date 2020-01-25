b6d61a24cd7ce49e93ddad897f37b6b32399f9ba
Leah Tynan <brunetto@alum.mit.edu>
2020-01-25 16:18:47 -0500

    Created Steph's whale animation, assigned assets to tiles

--- Assets/Scripts/PuzzlePieceManager.cs
+++ Assets/Scripts/PuzzlePieceManager.cs
@@ -7,7 +7,7 @@ public class PuzzlePieceManager : MonoBehaviour {
 	public PuzzlePieceViewer viewer;
 	public bool isSet; // Whether or not the piece has been rotated to the correct slot
 	public int initialSlot; // The image when the puzzle loads, never the correct image. These will be curated and set on the Inspector.
-	public int currentSlot; // The image currently visible to the user
+	public int currentSlot; // The image currently visible to the user. Note: Value here on the Inspector is meaningless, whereas initialSlot and correctSlot assignments are. This is public so the viewer can access it.
 	public int correctSlot; // The image in the series of rotating slots that is the correct fit for the puzzle. This should also be set in the Inspector.
 
 	// Use this for initialization
@@ -40,7 +40,7 @@ public class PuzzlePieceManager : MonoBehaviour {
 		isSet = true; 
 		gameManager.numberPiecesPlaced++;
 		gameManager.PlayNote();
-		viewer.SetPiece();
+		// viewer.SetPiece(); TODO: In the future, may add a UI update to indicate the piece has been locked
 		Debug.Log(">>> Piece was set");
 	}
 
